528b53e2b6576c692bd7b0443175de197057ae8e
Artem <85881416+norim-code@users.noreply.github.com>
2023-02-17 21:37:40 +0300

    Downler v2.0
    GUI

--- downler.py
+++ downler.py
@@ -1,13 +1,32 @@
+import sys
+from PySide6.QtWidgets import (QLineEdit, QPushButton, QApplication,
+    QVBoxLayout, QDialog)
 from pytube import YouTube
 
-def download_video(link):
-    video = YouTube(link)
-    video = video.streams.get_highest_resolution()
-    try:
-        video.download()
-    except:
-        print('Error!')
-    print('Successfully!')
+class Form(QDialog):
 
-link = input('Type link: ')
-download_video(link)
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        # Create widgets
+        self.edit = QLineEdit("Link")
+        self.button = QPushButton("Download")
+        # Create layout and add widgets
+        layout = QVBoxLayout()
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+        # Set dialog layout
+        self.setLayout(layout)
+        # Add button signal to greetings slot
+        self.button.clicked.connect(self.download_video)
+    def download_video(self):
+        video = YouTube(self.edit.text())
+        video = video.streams.get_highest_resolution()
+        video.download()
+if __name__ == '__main__':
+    # Create the Qt Application
+    app = QApplication(sys.argv)
+    # Create and show the form
+    form = Form()
+    form.show()
+    # Run the main Qt loop
+    sys.exit(app.exec())
